0c5c2522236ec3fb02b6bd60180980d41ef1ca96
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-02-04 19:52:15 -0600

    funciona chat, los estilos estan raros

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var socket = require('socket.io');
 
 // App setup 
-var app = express()
+var app = express();
 var server = app.listen(4000,function(){
     console.log('listening to request on port 4000');
 });
@@ -13,6 +13,14 @@ app.use(express.static('public'));
 // Socket setup
 var io = socket(server);
 
-io.on('connection',function(socket){
-    console.log('made socket connection',socket.id);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
 });
