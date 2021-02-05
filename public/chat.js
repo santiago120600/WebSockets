faabce7c3a32fd0a49f6308426413079d04ab02a
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-02-05 14:24:43 -0600

    fixed styles

--- public/chat.js
+++ public/chat.js
@@ -18,5 +18,5 @@ btn.addEventListener('click',function(){
 
 //Listen for events
 socket.on('chat',function(data){
-    output.innerHTML += '<p><strong>'+data.handle+'</strong>'+data.message+'</p>'
+    output.innerHTML += '<p><strong>'+data.handle+':</strong> '+data.message+'</p>'
 });
